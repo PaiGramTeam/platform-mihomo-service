6e0f3f18739f0fa9ab1ac9253e21ab0bf5e95e12
luoshui <luoshuijs@outlook.com>
2026-04-17 09:58:00 +0800

    :sparkles: add grpc health endpoint
    Signed-off-by: luoshui <luoshuijs@outlook.com>

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -7,11 +7,18 @@ import (
 	platformv1 "github.com/PaiGramTeam/proto-contracts/platform/v1"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	kratosgrpc "github.com/go-kratos/kratos/v2/transport/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 
 	mihomoapiv1 "platform-mihomo-service/api/mihomo/v1"
 	"platform-mihomo-service/internal/conf"
 	"platform-mihomo-service/internal/service"
 )
+
+type serviceInfoProvider interface {
+	GetServiceInfo() map[string]grpc.ServiceInfo
+}
 
 func NewGRPCServer(
 	bc *conf.Bootstrap,
@@ -27,6 +34,7 @@ func NewGRPCServer(
 		kratosgrpc.Timeout(time.Duration(grpcConf.GetTimeoutSeconds())*time.Second),
 		kratosgrpc.Middleware(recovery.Recovery()),
 	)
+	registerHealthServer(srv)
 
 	if mihomoSvc != nil {
 		mihomoapiv1.RegisterMihomoAccountServiceServer(srv, mihomoSvc)
@@ -39,4 +47,16 @@ func NewGRPCServer(
 	}
 
 	return srv
+}
+
+func registerHealthServer(registrar grpc.ServiceRegistrar) {
+	if serviceInfo, ok := registrar.(serviceInfoProvider); ok {
+		if _, exists := serviceInfo.GetServiceInfo()[healthpb.Health_ServiceDesc.ServiceName]; exists {
+			return
+		}
+	}
+
+	healthServer := health.NewServer()
+	healthServer.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
+	healthpb.RegisterHealthServer(registrar, healthServer)
 }
